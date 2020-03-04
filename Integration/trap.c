5e5b8ab549b13e5fc5b0f99f869241328f897d4c
mmc378 <mmc378@xunil-03.coe.drexel.edu>
2020-03-04 10:37:12 -0500

    working version of num_int; need to check w TA for accuracy

--- Integration/trap.c
+++ Integration/trap.c
@@ -14,12 +14,28 @@
  *
  */
 
+#define _REENTRANT /* Make sure the library functions are MT (muti-thread) safe */
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <pthread.h>
 #include <string.h>
 #include <math.h>
 #include <float.h>
 #include <time.h>
+
+/* Function prototype for the thread routines */
+void *my_func (void *);
+
+/* Structure used to pass arguments to the worker threads */
+typedef struct args_for_thread_t {
+    int num_threads;        /* Number of worker threads */
+    int tid;                /* Thread ID */
+    int arg1;               /* First argument */
+    float arg2;             /* Second argument */
+    double *partial_integral;    /* Third argument */
+} ARGS_FOR_THREAD; 
 
 double compute_using_pthreads (float, float, int, float, int);
 double compute_gold (float, float, int, float);
@@ -36,21 +52,28 @@ main (int argc, char **argv)
         exit (EXIT_FAILURE);
     }
 
-    float a = atoi (argv[1]); /* Lower limit */
+  float a = atoi (argv[1]); /* Lower limit */
 	float b = atof (argv[2]); /* Upper limit */
 	float n = atof (argv[3]); /* Number of trapezoids */
 
 	float h = (b - a)/(float) n; /* Base of each trapezoid */  
 	printf ("The base of the trapezoid is %f\n", h);
 
+    struct timeval start, stop;	
+	gettimeofday (&start, NULL);
 	double reference = compute_gold (a, b, n, h);
+    gettimeofday (&stop, NULL);
     printf ("Reference solution computed using single-threaded version = %f\n", reference);
-
+    printf ("Execution time = %fs\n", (float)(stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/(float)1000000));
+	printf ("\n");
 	/* Write this function to complete the trapezoidal rule using pthreads. */
     int num_threads = atoi (argv[4]); /* Number of threads */
+    gettimeofday (&start, NULL);
 	double pthread_result = compute_using_pthreads (a, b, n, h, num_threads);
-	printf ("Solution computed using %d threads = %f\n", num_threads, pthread_result);
-
+	gettimeofday (&stop, NULL);
+    printf ("Solution computed using %d threads = %f\n", num_threads, pthread_result);
+    printf ("Execution time = %fs\n", (float)(stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/(float)1000000));
+	printf ("\n");
     exit (EXIT_SUCCESS);
 } 
 
@@ -94,9 +117,57 @@ compute_gold (float a, float b, int n, float h)
 double 
 compute_using_pthreads (float a, float b, int n, float h, int num_threads)
 {
-	double integral = 0.0;
+	double integral = (f(a) + f(b))/2.0;
+ 
+    //added from vector_dot_product_v1
+    pthread_t *thread_id;                       /* Data structure to store the thread IDs */
+    pthread_attr_t attributes;                  /* Thread attributes */
+    pthread_attr_init(&attributes);             /* Initialize thread attributes to default values */
+    int i;
 
+    /* Allocate memory on heap for data structures and create worker threads */
+    thread_id = (pthread_t *) malloc (sizeof (pthread_t) * num_threads);
+    double *partial_integral = (double *) malloc (sizeof (double) * num_threads);
+    ARGS_FOR_THREAD *args_for_thread = (ARGS_FOR_THREAD *) malloc (sizeof (ARGS_FOR_THREAD) * num_threads);
+
+    for (i = 0; i < num_threads; i++) {
+        args_for_thread[i].num_threads = num_threads;
+        args_for_thread[i].tid = i; 
+        args_for_thread[i].arg1 = a; 
+        args_for_thread[i].arg2 = h; 
+        args_for_thread[i].partial_integral = partial_integral; 
+    }
+
+    for (i = 0; i < num_threads; i++)
+    pthread_create (&thread_id[i], &attributes, my_func, (void *) &args_for_thread[i]);
+					 
+    /* Wait for workers to finish */
+    for (i = 0; i < num_threads; i++)
+        pthread_join (thread_id[i], NULL);
+		 
+    /* Accumulate partial integrals computed by worker threads */ 
+    for (i = 0; i < num_threads; i++){
+        integral += partial_integral[i];
+    }
+
+    integral=integral*h;
     return integral;
+}
+
+/* Function that will be executed by all the worker threads */
+void *
+my_func (void *this_arg)
+{
+    ARGS_FOR_THREAD *args_for_me = (ARGS_FOR_THREAD *) this_arg; /* Typecast argument passed to function to appropriate type */
+
+    /* Compute partial sum that this thread is responsible for */
+    double partial_integral = 0.0; 
+	partial_integral=f(args_for_me->arg1+args_for_me->tid*args_for_me->arg2);
+
+    /* Store partial integrals into the partial_sum array */
+    args_for_me->partial_integral[args_for_me->tid] = (float) partial_integral;
+    //printf ("Thread %d is done \n", args_for_me->tid);
+    pthread_exit ((void *) 0);
 }
 
 
